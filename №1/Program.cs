4ec267d07825b102db817c9733c6a672cf4de35e
Goose <sanya.vaganov.96@mail.ru>
2024-02-07 10:24:02 +0700

    Задание выполнил

--- №1/Program.cs
+++ №1/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.WriteLine("Введите значение M:");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение N:");
+int n = int.Parse(Console.ReadLine());
+PrintNaturalNumbers(m, n);
+Console.ReadLine();
+
+static void PrintNaturalNumbers(int m, int n)
+{
+    if (m <= n)
+    {
+        Console.WriteLine(m);
+        PrintNaturalNumbers(m + 1, n);
+    }
+}
--- №3/Program.cs
+++ №3/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        PrintArrayReverse(arr, arr.Length - 1);
+    }
+
+static void PrintArrayReverse(int[] arr, int index)
+    {
+        if (index >= 0)
+        {
+            Console.WriteLine(arr[index]);
+            PrintArrayReverse(arr, index - 1);
+        }
+    }
+}
